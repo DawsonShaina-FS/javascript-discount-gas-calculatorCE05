bfa136554c83396363289b0a44fbf5e7829f9b63
Shaina Dawson <shainadawson@Shainas-MacBook-Air.local>
2026-03-22 04:18:10 -0400

    feat: add fuel checker, input validation, and dynamic UI interactions

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+//DISCOUNT CHECKER AND CALCULATOR
+
+
 //initial alert welcoming the user 
 alert("Hello and welcome to the Discount Double Check!");
 
@@ -19,29 +22,44 @@ if (isNaN(firstItem) || isNaN(secondItem)) {
 }
 else if (total >= 100) {
     let finalCost = 0.90 * total;
-    document.querySelector("#finalTotal").innerHTML = `Your total purchase is ${finalCost.toFixed(2)} which includes a 10% discount!`;
+    document.querySelector("#finalTotal").innerHTML = `Your total purchase is $${finalCost.toFixed(2)} which includes a 10% discount!`;
     document.querySelector("#finalTotal").className = "firstclass";
 }
 else if (total >= 50 && total < 100) {
     let finalCost = 0.95 * total;
-    document.querySelector("#finalTotal").innerHTML = `Your total purchase is ${finalCost.toFixed(2)} which includes a 5% discount!`;
+    document.querySelector("#finalTotal").innerHTML = `Your total purchase is $${finalCost.toFixed(2)} which includes a 5% discount!`;
     document.querySelector("#finalTotal").className = "firstclass";
 }
 else if (total < 50) {
     let finalCost = total;
-    document.querySelector("#finalTotal").innerHTML = `Your total purchase is ${finalCost.toFixed(2)}.`;
+    document.querySelector("#finalTotal").innerHTML = `Your total purchase is $${finalCost.toFixed(2)}`;
     document.querySelector("#finalTotal").className = "secondclass";
 }
 
-//FUEL CALCULATOR
+//FUEL CHECKER AND CALCULATOR 
 
 alert("Lets find out if you can cross the desert with your current gas level, or if you need to stop for gas!")
 
 let tankCapacity = parseInt(prompt("What is the maximum number of gallons your car's gas tank can hold?"));
 
-let currentGas = parseFloat(prompt("How full is your gas tank (in %"));
+let currentGas = parseFloat(prompt("How full is your gas tank (in %)", "ex: 50"));
 
 let mPG = parseInt(prompt("How many miles per gallon does your car get?"));
 
 let milesLeft = tankCapacity * (currentGas / 100) * mPG
+
+if (isNaN(tankCapacity) || isNaN(currentGas) || isNaN(mPG)) {
+    alert("Invalid input, please enter valid numbers.");
+}
+else if (milesLeft >= 200) {
+    document.querySelector("#showGraph").addEventListener("click", function (e) { document.querySelector("#picture").style.visibility = "visible"; document.querySelector("#textResults").innerHTML = ""; });
+
+    document.querySelector("#showText").addEventListener("click", function (e) { document.querySelector("#textResults").innerHTML = `Yes you can drive ${milesLeft} and you can make it without stopping for gas!`; document.querySelector("#picture").style.visibility = "hidden" });
+
+}
+else if (milesLeft < 200) {
+    document.querySelector("#showGraph").addEventListener("click", function (e) { document.querySelector("#picture").style.visibility = "visible"; document.querySelector("#picture").src = "IMG/notEnoughGas.jpg"; document.querySelector("#textResults").innerHTML = ""; });
+
+    document.querySelector("#showText").addEventListener("click", function (e) { document.querySelector("#textResults").innerHTML = `You can only drive ${milesLeft} more miles! Better get gas soon!`; document.querySelector("#picture").style.visibility = "hidden" });
+}
 
