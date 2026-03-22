9c3c3822a770836ea9d2816d768d9a406ddb0345
Shaina Dawson <shainadawson@Shainas-MacBook-Air.local>
2026-03-22 00:25:31 -0400

    feat: add user prompts and basic UI styling

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-alert("file is connected!")
+//initial alert welcoming the user 
+alert("Hello and welcome to the Discount Double Check!");
+
+//alert explaining what is going to be done
+alert("I'm going to ask you for the price of two items, then calculate your total including any discounts that apply!");
+
+//prompting the user for the cost of the first item
+let firstItem = parseFloat(prompt("What is the cost of the first item?", "Ex: 2.50"));
+
+//prompting the user for the cost of the second item
+let secondItem = parseFloat(prompt("What is the cost of the second item?", "ex: 4.00"));
+
